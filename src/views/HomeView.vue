98dd2e09f3a661156c3cb07db941113dfc70bf1d
William Stam <william@munsoft.co.za>
2022-09-04 09:29:16 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Anything plants... coming soon
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss">
+body {
+    background: #231f20;
+}
+</style>
