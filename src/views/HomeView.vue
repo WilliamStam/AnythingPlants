da03b5ed93b0fe1e8c4d19902992e9244f4d4757
William Stam <william@munsoft.co.za>
2022-09-05 09:21:00 +0200

    green

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,8 @@
 <template>
 
-            <div class="d-flex aligns-items-center justify-content-center card text-center  position-absolute top-50 start-50 translate-middle bg-white shadow-lg p-4" style="border:#ccc 1px solid">
-            <div class="card-header ">
-                Anything Plants
-            </div>
-            <div class="card-body">
-               ... coming soon ...
+            <div class="d-flex aligns-items-center justify-content-center card text-center  position-absolute top-50 start-50 translate-middle bg-white shadow-lg border">
 
-            </div>
-                <div class="card-footer text-bold align-content-center text-light">
+                <div class="text-bold align-content-center m-5">
                     <span>info</span><fa icon="fa-solid fa-at"></fa><span>anythingplants.co.za</span>
                 </div>
         </div>
